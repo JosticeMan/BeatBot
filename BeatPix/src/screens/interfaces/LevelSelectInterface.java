7497622fa7f00320d650f8e666e2166860b71fd7
TylerOvenden <tovenden4386@bths.edu>
2018-02-15 11:44:43 -0500

    Update LevelSelectInterface.java

--- BeatPix/src/screens/interfaces/LevelSelectInterface.java
+++ BeatPix/src/screens/interfaces/LevelSelectInterface.java
@@ -1,18 +1,12 @@
+
 package screens.interfaces;
 
-import gui.components.Button;
-
 public interface LevelSelectInterface {
-	/*
-	 * author: Tyler Ovenden
-	 */
-	public static void updateDisplay() {
+
+	public static void moveLeft() {
 		
 	}
-	public static void updateList() {
-		
-	}
-	public static void flairButton(Button b) {
+	public static void moveRight() {
 		
 	}
 }
